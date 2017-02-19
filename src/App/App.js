5cb166ee3e5ad3be985eea623a490ef223ba909d
Azzapop <a.b.hook@live.com.au>
2017-02-19 16:47:03 +1100

    open and close square functionality

--- src/App/App.js
+++ src/App/App.js
@@ -1,36 +1,59 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import _ from 'lodash';
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      topLeftStatus: 'active',
-      topRightStatus: 'active',
-      bottomLeftStatus: 'active',
-      bottomRightStatus: 'active',
+      topLeft: 'off-split',
+      topRight: 'off-split',
+      bottomLeft: 'off-split',
+      bottomRight: 'off-split',
     }
   }
 
-  changeActive(box) {
-    const state = this.state[box];
-    this.setState({
-      topLeftStatus: 'inactive',
-      topRightStatus: 'inactive',
-      bottomLeftStatus: 'inactive',
-      bottomRightStatus: 'inactive',
-      [box]: (state === 'enlarged' ? 'inactive' : 'enlarged'),
+  enlarge(square) {
+    const { state } = this;
+    let newState = {};
+    _.map(state, (v, k) => {
+      if (k === square) newState[k] = "split-full";
+      else newState[k] = "split-off";
     });
+    this.setState(newState);
+  }
+
+  close() {
+    const { state } = this;
+    let newState = {};
+    _.map(state, (v, k) => {
+      if (v === "split-full") newState[k] = "full-split";
+      else newState[k] = "off-split";
+    });
+    this.setState(newState);
   }
 
   render() {
-    const { topLeftStatus, topRightStatus, bottomLeftStatus, bottomRightStatus } = this.state;
+    const { topLeft, topRight, bottomLeft, bottomRight } = this.state;
     return (
       <div>
-        <div onClick={() => this.changeActive("topLeftStatus")} className={ "home-square top-left " + topLeftStatus }></div>
-        <div onClick={() => this.changeActive("topRightStatus")} className={ "home-square top-right " + topRightStatus }></div>
-        <div onClick={() => this.changeActive("bottomLeftStatus")} className={ "home-square bottom-left " + bottomLeftStatus }></div>
-        <div onClick={() => this.changeActive("bottomRightStatus")} className={ "home-square bottom-right " + bottomRightStatus }></div>
+        <div className={ "home-square top-left " + topLeft }>
+          <p onClick={ () => this.enlarge("topLeft") }>Open</p>
+          <p onClick={ () => this.close() }>Close</p>
+        </div>
+        <div className={ "home-square top-right " + topRight }>
+          <p onClick={ () => this.enlarge("topRight") }>Open</p>
+          <p onClick={ () => this.close() }>Close</p>
+        </div>
+        <div className={ "home-square bottom-left " + bottomLeft }>
+          <p onClick={ () => this.enlarge("bottomLeft") }>Open</p>
+          <p onClick={ () => this.close() }>Close</p>
+        </div>
+        <div className={ "home-square bottom-right " + bottomRight }>
+          <p onClick={ () => this.enlarge("bottomRight") }>Open</p>
+          <p onClick={ () => this.close() }>Close</p>
+        </div>
       </div>
     );
   }
