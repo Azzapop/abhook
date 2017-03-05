7cf5aa4ee234f465ca78517fb4d2c94e3a5bc137
Azzapop <a.b.hook@live.com.au>
2017-03-06 09:15:39 +1100

    add/remove correct content at correct time

--- website/client/app/bundles/Portfolio/components/Portfolio.jsx
+++ website/client/app/bundles/Portfolio/components/Portfolio.jsx
@@ -35,23 +35,40 @@ class App extends Component {
 
   render() {
     const { topLeft, topRight, bottomLeft, bottomRight } = this.state;
+    let topLeftContent, topRightContent, bottomLeftContent, bottomRightContent;
+    if (topLeft === "off-split" || topLeft === "full-split") {
+      topLeftContent = <p onClick={ () => this.enlarge("topLeft") }>Open</p>;
+    } else if (topLeft === "split-full") {
+      topLeftContent = <p onClick={ () => this.close() }>Close</p>;
+    }
+    if (topRight === "off-split" || topRight === "full-split") {
+      topRightContent = <p onClick={ () => this.enlarge("topRight") }>Open</p>;
+    } else if (topRight === "split-full") {
+      topRightContent = <p onClick={ () => this.close() }>Close</p>;
+    }
+    if (bottomLeft === "off-split" || bottomLeft === "full-split") {
+      bottomLeftContent = <p onClick={ () => this.enlarge("bottomLeft") }>Open</p>;
+    } else if (bottomLeft === "split-full") {
+      bottomLeftContent = <p onClick={ () => this.close() }>Close</p>;
+    }
+    if (bottomRight === "off-split" || bottomRight === "full-split") {
+      bottomRightContent = <p onClick={ () => this.enlarge("bottomRight") }>Open</p>;
+    } else if (bottomRight === "split-full") {
+      bottomRightContent = <p onClick={ () => this.close() }>Close</p>;
+    }
     return (
       <div>
         <div className={ "home-square top-left " + topLeft }>
-          <p onClick={ () => this.enlarge("topLeft") }>Open</p>
-          <p onClick={ () => this.close() }>Close</p>
+          { topLeftContent }
         </div>
         <div className={ "home-square top-right " + topRight }>
-          <p onClick={ () => this.enlarge("topRight") }>Open</p>
-          <p onClick={ () => this.close() }>Close</p>
+          { topRightContent }
         </div>
         <div className={ "home-square bottom-left " + bottomLeft }>
-          <p onClick={ () => this.enlarge("bottomLeft") }>Open</p>
-          <p onClick={ () => this.close() }>Close</p>
+          { bottomLeftContent }
         </div>
         <div className={ "home-square bottom-right " + bottomRight }>
-          <p onClick={ () => this.enlarge("bottomRight") }>Open</p>
-          <p onClick={ () => this.close() }>Close</p>
+          { bottomRightContent }
         </div>
       </div>
     );
