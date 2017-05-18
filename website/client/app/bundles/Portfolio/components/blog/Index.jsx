a9d848cf82dd5bd19c002fec7295a5b6939ac384
Azzapop <aaron.b.hook@gmail.com>
2017-05-18 21:59:54 +1000

    finish blog index

--- website/client/app/bundles/Portfolio/components/blog/Index.jsx
+++ website/client/app/bundles/Portfolio/components/blog/Index.jsx
@@ -1,23 +1,48 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 
+import moment from 'moment';
 import _ from 'lodash';
 
 class Index extends Component {
   constructor(props) {
     super(props);
     const { posts } = props;
-    const sorted = _.groupBy(posts, (post) => { return post.pinned; });
     this.state = {
-      pinned: sorted.true || [],
-      posts: sorted.false || []
+      posts: _.orderBy(posts, ['pinned', 'created_at'], ['desc', 'desc'])
     }
   }
 
+  renderPosts(posts) {
+    return _.map(posts, (post) => {
+      return (
+        <div key={ post.id } className='row blog-post'>
+          <Link to={ '/blog/' + post.slug } className='blog-link'>
+            <div className='box col-1 blog-icon'>
+              <i className={ 'fa fa-2x ' + (post.pinned ? 'fa-thumb-tack fa-rotate-45' : 'fa-envelope') }></i>
+            </div>
+            <div className='box col-11 blog-context'>
+              <h5>{ post.title }</h5>
+              <p className='blog-dates'>
+                <span>Published: { moment(post.created_at).format('MMM Do YYYY, HH:mm') }</span>
+                <span>Last updated: { moment(post.updated_at).format('MMM Do YYYY, HH:mm') }</span>
+              </p>
+              <p>{ post.blurb }</p>
+            </div>
+          </Link>
+        </div>
+      );
+    });
+  }
+
   render() {
-    const { pinned, posts } = this.state;
+    const { posts } = this.state;
     return (
-      <Link to='/blog/test-post'>Go to post</Link>
+      <div className='container'>
+        {
+          this.renderPosts(posts)
+        }
+      </div>
     );
   }
 }
