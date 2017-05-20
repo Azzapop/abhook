d2f1b14a7785919e42d8b02189f37e3c4b5621c0
Azzapop <aaron.b.hook@gmail.com>
2017-05-20 16:43:35 +1000

    finish creating individual post page

--- website/client/app/bundles/Portfolio/components/blog/Post.jsx
+++ website/client/app/bundles/Portfolio/components/blog/Post.jsx
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
+import Linkify from 'react-linkify';
+import { Link } from 'react-router';
+
+import moment from 'moment';
+import _ from 'lodash';
 
 class Post extends Component {
   constructor(props) {
     super(props);
+    const { posts } = props;
+    const { postId } = props.params;
+    this.state = {
+      post: _.find(posts, (p) => { return p.slug === postId })
+    };
   }
 
   render() {
-    return <p>{ this.props.params.post_id }</p>;
+    const { post } = this.state;
+    const content = _.map(post.content.split(/\n/g), (para, i) => {
+      return (
+        <div className='row' key={ i }>
+          <Linkify properties={ { target: '_blank' } }>
+            <p className='paragraph'>{ para }</p>
+          </Linkify>
+        </div>
+      );
+    });
+    return (
+      <div className='container'>
+        <h5>{ post.title }</h5>
+        <p className='blog-dates'>
+          <span>Published: { moment(post.created_at).format('MMM Do YYYY, HH:mm') }</span>
+          <span>Last updated: { moment(post.updated_at).format('MMM Do YYYY, HH:mm') }</span>
+        </p>
+        { content }
+        <Link to="/blog"><i className='fa fa-long-arrow-left'></i> Back to blog</Link>
+      </div>
+    );
   }
 }
 
