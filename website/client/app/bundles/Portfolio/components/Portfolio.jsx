3b0d9fbfbc933b7848d4148ee43ae0f8e070edc5
Azzapop <aaron.b.hook@gmail.com>
2017-05-21 17:13:00 +1000

    begin portfolio page

--- website/client/app/bundles/Portfolio/components/Portfolio.jsx
+++ website/client/app/bundles/Portfolio/components/Portfolio.jsx
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
 
-import _ from 'lodash';
-
-class App extends Component {
+class Portfolio extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-    }
   }
 
   render() {
     return (
-      <p>Home Pagessssssssssssssssss</p>
+      <div className='container'>
+        <div className='row'>
+          <div className='box col-12'>
+            <img className='img thumbnail round left portrait' src='https://avatars3.githubusercontent.com/u/4386657?v=3&s=460' />
+            <h2 className='heading'>Aaron Hook</h2>
+            <h1 className='sub-heading'>Software Developer</h1>
+            <p>Hey there! Welcome to abhook.id.au! My name is Aaron Hook and I'm the owner of this fine establishment. In high school I began to learn the joy of coding from my teacher, and took every computer science course I possibly could. Once I had graduated high school it was only natural I continued my studies, recieving a scholarship from the University of Sydney. I graduated in 2016 with a Bachelor of Computer Science and Technology. Ever since I've been developing my skills and working to produce the best projects I can.</p>
+          </div>
+        </div>
+        <div className='row'>
+          <p>Showcase</p>
+        </div>
+      </div>
     );
   }
 }
 
-export default App;
+export default Portfolio;
