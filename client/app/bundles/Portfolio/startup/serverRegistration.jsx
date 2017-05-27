d25961fd7a6111057f26caffbd67a2df00974a64
Azzapop <aaron.b.hook@gmail.com>
2017-05-27 17:26:52 +1000

    add clock component

--- client/app/bundles/Portfolio/startup/serverRegistration.jsx
+++ client/app/bundles/Portfolio/startup/serverRegistration.jsx
@@ -3,10 +3,12 @@ import ReactOnRails from 'react-on-rails';
 import Portfolio from '../components/Portfolio';
 import Contact from '../components/Contact';
 import Blog from './ServerRouter';
+import Clock from '../components/Clock';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
   Portfolio,
   Contact,
   Blog,
+  Clock,
 });
