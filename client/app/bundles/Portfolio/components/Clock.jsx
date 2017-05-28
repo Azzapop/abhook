c3d622340f803fd46a081eca326d0382c6c0e76c
Azzapop <aaron.b.hook@gmail.com>
2017-05-28 17:16:13 +1000

    finish clock component

--- client/app/bundles/Portfolio/components/Clock.jsx
+++ client/app/bundles/Portfolio/components/Clock.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import moment from 'moment';
 
-const Clock = (props) => {
-  return (
-    <div>
-      <p>Clock</p>
-    </div>
-  );
-};
+class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: moment().format('ddd, DD MMM YYYY HH:mm:ss')
+    };
+  }
+
+  componentDidMount() {
+    this.timerId = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
+  tick() {
+    this.setState({
+      time: moment().format('ddd, DD MMM YYYY HH:mm:ss')
+    });
+  }
+
+  render() {
+    const { time } = this.state;
+    return <p className='date left'><i className='fa fa-calendar'></i> { time }</p>;
+  }
+}
 
 export default Clock;
