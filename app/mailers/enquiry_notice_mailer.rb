0e1a02246865369bcac8e87424d98f1db7990553
Azzapop <aaron.b.hook@gmail.com>
2017-06-25 13:23:19 +1000

    make from email the enquirer's email

--- app/mailers/enquiry_notice_mailer.rb
+++ app/mailers/enquiry_notice_mailer.rb
@@ -1,9 +1,8 @@
 class EnquiryNoticeMailer < ApplicationMailer
-  default from: "abhook.id.au enquiry <enquiries@abhook.id.au>"
   default to: "aaron.b.hook@gmail.com"
 
   def send_enquiry(enquiry_id)
     @enquiry = Enquiry.find(enquiry_id)
-    mail(subject: "New enquiry from #{ @enquiry.email }")
+    mail(from: @enquiry.email, subject: "New enquiry from #{ @enquiry.email }")
   end
 end
